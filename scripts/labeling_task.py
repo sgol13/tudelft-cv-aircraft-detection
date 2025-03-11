51d1f167d0f4a42a8c2b6da48c9e2c06c9150c3a
Szymon Golebiowski <szymon13gol@gmail.com>
2025-03-11 11:15:18 +0100

    fetching task content from spreadsheet

--- scripts/labeling_task.py
+++ scripts/labeling_task.py
@@ -1,22 +1,54 @@
 import argparse
 import os
 import shutil
+import re
+from typing import List
+
 from tqdm import tqdm
+import requests
+import csv
 
 TASK_VIDEOS_PATH = 'videos'
 
+def get_task_id(task_path: str) -> str:
+    dir_name = os.path.basename(os.path.normpath(task_path))
 
-def find_txt_file(directory: str) -> str:
-    for file in os.listdir(directory):
-        if file.endswith('.txt'):
-            return os.path.join(directory, file)
-    raise FileNotFoundError("No .txt file found in the directory")
+    try:
+        task_id = re.search(r'\d+$', dir_name).group()
+    except AttributeError:
+        raise ValueError(f"Cannot read task ID from the directory name {dir_name}")
 
+    return task_id
 
-def read_filenames_from_file(file_path: str):
-    with open(file_path, 'r') as file:
-        filenames = [line.strip() + '.mp4' for line in file]
-    return filenames
+def fetch_task_filenames_from_spreadsheet(task_id: str) -> List[str]:
+    file_id = "1hAqC2Pz3xIEqdslCMa91_bKWBSlOHUs2sL5UKU8Wr7c"
+    gid = "216287426"
+    csv_url = f"https://docs.google.com/spreadsheets/d/{file_id}/export?format=csv&gid={gid}"
+
+    response = requests.get(csv_url)
+    response.raise_for_status()  # Raise error if request fails
+
+    rows = list(csv.reader(response.text.splitlines()))
+
+    # for it, row in enumerate(rows):
+    #     print(it, row[:6])
+
+    try:
+        column_index = rows[0].index(task_id)
+    except ValueError:
+        raise ValueError(f"Task ID {task_id} not found in the first row.")
+
+    task_column = [row[column_index] for row in rows]
+
+    try:
+        videos_row_index = task_column.index('videos')
+    except ValueError:
+        raise ValueError(f"'videos' row not found in the task {task_id} column.")
+
+    task_content = task_column[videos_row_index + 1:]
+    task_content = [f'{v}.mp4' for v in task_content if v]
+
+    return task_content
 
 
 def copy_files_to_videos_dir(task_videos_dir: str, filenames: list, all_videos_dir: str):
@@ -46,12 +78,8 @@ def update_labeling_task(task_path: str, all_videos_dir: str):
     task_videos_dir = os.path.join(task_path, TASK_VIDEOS_PATH)
     os.makedirs(task_videos_dir, exist_ok=True)
 
-    print(task_path)
-    txt_file_path = find_txt_file(task_path)
-    filenames = read_filenames_from_file(txt_file_path)
-
-    print(task_videos_dir)
-    print(txt_file_path)
+    task_id = get_task_id(task_path)
+    filenames = fetch_task_filenames_from_spreadsheet(task_id)
 
     print("Copying videos...")
     copy_files_to_videos_dir(task_videos_dir, filenames, all_videos_dir)
