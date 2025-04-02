40ee0a6a09aefdfb3a0e33ea8d10ceb15764b425
Szymon Golebiowski <szymon13gol@gmail.com>
2025-04-02 13:58:38 +0200

    ultralytics live example copied

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,17 +1,110 @@
-import 'package:app/ui/camera_view.dart';
-import 'package:app/ui/debug_view.dart';
+import 'dart:io' as io;
+
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:ultralytics_yolo/ultralytics_yolo.dart';
+import 'package:ultralytics_yolo/yolo_model.dart';
 
 void main() {
-  runApp(ProviderScope(child: MyApp()));
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final controller = UltralyticsYoloCameraController();
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(body: Stack(children: [CameraView(), DebugView()])));
+    return MaterialApp(
+      home: Scaffold(
+        body: FutureBuilder<bool>(
+          future: _checkPermissions(),
+          builder: (context, snapshot) {
+            final allPermissionsGranted = snapshot.data ?? false;
+
+            return !allPermissionsGranted
+                ? const Center(
+              child: Text("Error requesting permissions"),
+            )
+                : FutureBuilder<ObjectDetector>(
+              future: _initObjectDetectorWithLocalModel(),
+              builder: (context, snapshot) {
+                final predictor = snapshot.data;
+
+                return predictor == null
+                    ? Container()
+                    : Stack(
+                  children: [
+                    UltralyticsYoloCameraPreview(
+                      controller: controller,
+                      predictor: predictor,
+                      onCameraCreated: () {
+                        predictor.loadModel(useGpu: true);
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Future<ObjectDetector> _initObjectDetectorWithLocalModel() async {
+    final modelPath = await _copy('assets/yolov8n_int8.tflite');
+    final metadataPath = await _copy('assets/yolov8n_int8_metadata.yaml');
+    final model = LocalYoloModel(
+      id: '',
+      task: Task.detect,
+      format: Format.tflite,
+      modelPath: modelPath,
+      metadataPath: metadataPath,
+    );
+
+    return ObjectDetector(model: model);
+  }
+
+  Future<String> _copy(String assetPath) async {
+    final path = '${(await getApplicationSupportDirectory()).path}/$assetPath';
+    await io.Directory(dirname(path)).create(recursive: true);
+    final file = io.File(path);
+    if (!await file.exists()) {
+      final byteData = await rootBundle.load(assetPath);
+      await file.writeAsBytes(byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+    }
+    return file.path;
+  }
+
+  Future<bool> _checkPermissions() async {
+    List<Permission> permissions = [];
+
+    var cameraStatus = await Permission.camera.status;
+    if (!cameraStatus.isGranted) permissions.add(Permission.camera);
+
+    var storageStatus = await Permission.photos.status;
+    if (!storageStatus.isGranted) permissions.add(Permission.photos);
+
+    if (permissions.isEmpty) {
+      return true;
+    } else {
+      try {
+        Map<Permission, PermissionStatus> statuses = await permissions.request();
+        return statuses.values.every((status) => status == PermissionStatus.granted);
+      } on Exception catch (_) {
+        return false;
+      }
+    }
   }
 }
+
