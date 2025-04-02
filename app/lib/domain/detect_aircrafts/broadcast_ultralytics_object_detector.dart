dc4cf64fda5dd0b0acefb0bc912ab930d2dee342
Szymon Golebiowski <szymon13gol@gmail.com>
2025-04-03 00:28:16 +0200

    broadcast stream

--- app/lib/domain/detect_aircrafts/broadcast_ultralytics_object_detector.dart
+++ app/lib/domain/detect_aircrafts/broadcast_ultralytics_object_detector.dart
@@ -3,4 +3,7 @@ import 'package:ultralytics_yolo/predict/detect/detect.dart';
 class BroadcastUltralyticsObjectDetector extends ObjectDetector {
   BroadcastUltralyticsObjectDetector({required super.model});
 
+  @override
+  Stream<List<DetectedObject?>?> get detectionResultStream =>
+      super.detectionResultStream.asBroadcastStream();
 }
