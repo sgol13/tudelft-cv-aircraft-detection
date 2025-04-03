6b84d2c7eb761f21b40f6a5c15d91b94889e58a6
Szymon Golebiowski <szymon13gol@gmail.com>
2025-04-03 15:30:05 +0200

    flutter camera preview

--- app/lib/ui/camera_view.dart
+++ app/lib/ui/camera_view.dart
@@ -1,6 +1,7 @@
 import 'package:app/domain/detect_aircrafts/detect_aircrafts.dart';
 import 'package:app/domain/detect_aircrafts/ultralytics_live_detect.dart';
 import 'package:app/ui/camera_view_annotator.dart';
+import 'package:app/ui/flutter_camera_preview.dart';
 import 'package:app/ui/ultralytics_camera_preview_wrapper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -14,14 +15,10 @@ class CameraView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // final cameraController = ref.watch(cameraProvider);
-    final ultralyticsLiveDetect = ref.watch(detectAircraftsProvider) as UltralyticsLiveDetect;
-
     return Stack(
       children: [
-        // if (cameraController != null && cameraController.value.isInitialized)
-        //   CameraPreview(cameraController),
-        UltralyticsCameraPreviewWrapper(),
+        FlutterCameraPreview(),
+        // UltralyticsCameraPreviewWrapper(),
 
         CameraViewAnnotator(),
       ],
